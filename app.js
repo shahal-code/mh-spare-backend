84c4d18b1204aa5702d65dbc72acd2b48e8a53a1
shahal-code <gmrshahl@gmail.com>
2026-08-09 11:55:22 +0530

    fix: increase express body parser limits to 50mb for large image uploads

--- app.js
+++ app.js
@@ -105,8 +105,8 @@ app.use(cors({
 
 app.use(morgan('combined'));
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(express.json({ limit: "50mb" }));
 app.use(helmet({
   crossOriginResourcePolicy: { policy: "cross-origin" }
 }));
